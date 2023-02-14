d14acce26fa86a766f547e8e2ed5d9f81f4f7c60
Alexander David Frick <Alex313031@gmail.com>
2023-02-14 14:10:14 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo apt-get install build-essential git libsecret-1-dev fakeroot rpm libx11-dev libxkbfile-dev nodejs npm node-gyp node-istanbul &&
+
+export ATOM_ELECTRON_URL='https://artifacts.electronjs.org/headers/dist' &&
+
 ./script/bootstrap &&
 
 ./script/build --create-debian-package --compress-artifacts
