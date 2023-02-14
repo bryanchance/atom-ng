b538a877da0c73d0975b98f703a8d09966fe164e
Alexander David Frick <Alex313031@gmail.com>
2023-02-14 14:22:13 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 sudo apt-get install build-essential git libsecret-1-dev fakeroot rpm libx11-dev libxkbfile-dev nodejs npm node-gyp node-istanbul &&
 
+export CFLAGS="-O3" &&
+export CXXFLAGS="-O3" &&
+export CPPFLAGS="-O3" &&
+export LDFLAGS="-Wl,-O3" &&
+
 export ATOM_ELECTRON_URL='https://artifacts.electronjs.org/headers/dist' &&
 
 ./script/bootstrap &&
