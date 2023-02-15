f4b6b43d116c952d40083ffda6e8ee36fe4a5e4d
Alexander David Frick <Alex313031@gmail.com>
2023-02-14 23:24:54 -0600

    fix build

--- build.sh
+++ build.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+# Copyright(c) 2023 Alex313031
+
+# Install prerequisites
 sudo apt-get install build-essential git libsecret-1-dev fakeroot rpm libx11-dev libxkbfile-dev nodejs npm node-gyp node-istanbul &&
 
+# Optimization parameters
 export CFLAGS="-O3" &&
 export CXXFLAGS="-O3" &&
 export CPPFLAGS="-O3" &&
 export LDFLAGS="-Wl,-O3" &&
 
+# Use upstream electron
 export ATOM_ELECTRON_URL='https://artifacts.electronjs.org/headers/dist' &&
 
+# Workaround for jasmine
+mkdir -p $HOME/.atom/.node-gyp &&
+cp -v gitconfig $HOME/.atom/.node-gyp/.gitconfig &&
+
+# Run final bootstrap
 ./script/bootstrap &&
 
+# Build linux package and archive for distribution
 ./script/build --create-debian-package --compress-artifacts
+
+exit 0
