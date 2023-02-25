6c5c3626dc62df982b6e528ca115eac4ccd2b581
Alexander David Frick <Alex313031@gmail.com>
2023-02-25 02:52:04 -0600

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,11 @@
+set CFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough
+set CXXFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough
+set CPPFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough
+set LDFLAGS=-Wl,-O3 -mavx -maes
+
+mkdir %USERPROFILE%\.atom\.node-gyp
+copy gitconfig %USERPROFILE%\.atom\.node-gyp\.gitconfig
+
+script\bootstrap.cmd
+
+script\build.cmd
