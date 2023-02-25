83fb6f863dbd279ece1296177f2a943a99c3788b
Alexander David Frick <Alex313031@gmail.com>
2023-02-25 04:42:00 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,11 +1,9 @@
-set CFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough
-set CXXFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough
-set CPPFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough
-set LDFLAGS=-Wl,-O3 -mavx -maes
+set CFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough &
+set CXXFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough &
+set CPPFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough &
+set LDFLAGS=-Wl,-O3 -mavx -maes &
 
-mkdir %USERPROFILE%\.atom\.node-gyp
-copy gitconfig %USERPROFILE%\.atom\.node-gyp\.gitconfig
-
-script\bootstrap.cmd
+mkdir %USERPROFILE%\.atom\.node-gyp &
+copy gitconfig %USERPROFILE%\.atom\.node-gyp\.gitconfig &
 
 script\build.cmd
