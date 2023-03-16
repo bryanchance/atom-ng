30c416c6912a57be1e971dc07936ab01199e5bed
Alexander David Frick <Alex313031@gmail.com>
2023-03-16 06:02:22 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
-set CFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough &
-set CXXFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough &
-set CPPFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough &
-set LDFLAGS=-Wl,-O3 -mavx -maes &
+set CFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-cast-function-type &
+set CXXFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-cast-function-type &
+set CPPFLAGS=-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-cast-function-type &
+set LDFLAGS=-Wl,-O3 -mavx -maes -s &
 
 mkdir %USERPROFILE%\.atom\.node-gyp &
 copy gitconfig %USERPROFILE%\.atom\.node-gyp\.gitconfig &
 
-script\build.cmd
+script\build.cmd --compress-artifacts
