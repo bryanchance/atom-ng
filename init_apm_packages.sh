f5dba2f7dc044b2e927ff6d2a2df132a29277d77
Alexander Frick <alex@pop-os.localdomain>
2023-04-03 20:49:27 -0500

    add dot-atom init script

---
+++ init_apm_packages.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Copyright(c) 2023 Alex313031
+
+YEL='\033[1;33m' # Yellow
+CYA='\033[1;96m' # Cyan
+RED='\033[1;31m' # Red
+GRE='\033[1;32m' # Green
+c0='\033[0m' # Reset Text
+bold='\033[1m' # Bold Text
+underline='\033[4m' # Underline Text
+
+# Error handling
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "${RED}Failed $*"; }
+
+export ELECTRON_CACHE="${PWD}/electron" &&
+export electron_config_cache="${PWD}/electron" &&
+
+# --help
+displayHelp () {
+	printf "\n" &&
+	printf "${bold}${GRE}Script to bootstrap \`dot-atom/packages\`${c0}\n" &&
+	printf "\n"
+}
+case $1 in
+	--help) displayHelp; exit 0;;
+esac
+
+# Optimization parameters
+export CFLAGS="-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-cast-function-type" &&
+export CXXFLAGS="-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-cast-function-type" &&
+export CPPFLAGS="-DNDEBUG -mavx -maes -O3 -g0 -s -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-cast-function-type" &&
+export LDFLAGS="-Wl,-O3 -mavx -maes -s" &&
+export VERBOSE=1 &&
+export V=1 &&
+
+# Put electron binaries here
+export ELECTRON_CACHE="${PWD}/electron" &&
+export electron_config_cache="${PWD}/electron" &&
+
+printf "\n" &&
+printf "${bold}${GRE} Bootstrapping packages with \`npm install\`...${c0}\n" &&
+printf "\n" &&
+
+cd ./dot-atom/packages &&
+cd atom-ng-browser &&
+npm install && cd .. &&
+cd color-picker &&
+npm install && cd .. &&
+cd linter &&
+npm install && npm run build && npm run build-commit && cd .. &&
+cd minimap &&
+npm install && npm run build && npm run build-commit && cd .. &&
+cd ..
+exit 0
