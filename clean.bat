c7fabe9a28922d4a009e87d52f9a589cddeb06b4
Alexander Frick <Alex313031@gmail.com>
2023-04-05 15:39:05 -0500

    clean win

---
+++ clean.bat
@@ -0,0 +1,3 @@
+echo Cleaning Atom-ng...
+
+npm run clean
--- init_apm_packages.bat
+++ init_apm_packages.bat
@@ -7,19 +7,10 @@ set LDFLAGS=-Wl,-O3 -mavx -maes -s
 set VERBOSE=1
 set V=1
 
+set MSVS_VERSION=2017
+set GYP_MSVS_VERSION=2017
+
 set ELECTRON_CACHE=%~dp0%electron\bin
 set electron_config_cache=%~dp0%electron\bin
 
-cd dot-atom\packages
-cd atom-ng-browser
-npm install
-cd ..
-cd color-picker
-npm install
-cd ..
-cd minimap
-npm install
-npm run build
-npm run build-commit
-cd ..
-cd ..
+cd dot-atom\packages&&cd atom-ng-browser&&npm install&&cd ..&&cd color-picker&&npm install&&cd ..&&cd minimap&&npm install&&npm run build&&npm run build-commit&&cd ..&&cd ..
